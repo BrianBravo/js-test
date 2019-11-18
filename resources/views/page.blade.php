8aaae6afbd4b4b250ca50ad0e7375bb8c63ecc84
Bravo <brianbravo@me.com>
2019-11-18 11:59:52 -0600

    First commit. All the things!

---
+++ resources/views/page.blade.php
@@ -0,0 +1,372 @@
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    <section class="section section--hero">
+        <div class="container">
+            <div class="columns columns--2">
+                <div class="column">
+                    <h1 class="heading">
+                        Launch your Amazon FBA business with the right data
+                    </h1>
+                    <p class="intro">
+                        Jungle Scout is the only tool that Amazon FBA sellerts trust for product research.
+                    </p>
+                </div>
+                <div class="column">
+                    <blockquote class="quote">
+                        <p>
+                            "Jungle Scout is the ONE tool I cannot live without. There's ways to use it that simply blow you away once you realize the power of what they can do for your business. A must-have tool for any Amazon seller!"
+                        </p>
+                        <div class="media">
+                            <figure>
+                                <img src="@asset('images/scott-img.png')" alt="Scott Volker">
+                            </figure>
+                            <div class="media__text">
+                                <p>
+                                    Scott Volker
+                                </p>
+                                <small>
+                                    The Amazing Seller
+                                </small>
+                            </div>
+                        </div>
+                    </blockquote>
+                </div>
+            </div>
+            <ul class="testimonials">
+                <li class="testimonials__item">
+                    <p>
+                        "The most helpful Amazon software in the space"
+                    </p>
+                    <img src="@asset('images/huffpost-logo.png')" alt="">
+                </li>
+                <li class="testimonials__item">
+                    <p>
+                        "The best resource for data-driven product research"
+                    </p>
+                    <img src="@asset('images/forbes-logo.png')" alt="">
+                </li>
+                <li class="testimonials__item">
+                    <p>
+                        "Handles the heavy lifting on big data"
+                    </p>
+                    <img src="@asset('images/entrepreneur-logo.png')" alt="">
+                </li>
+                <li class="testimonials__item">
+                    <p>
+                        "The most helpful Amazon software in the space"
+                    </p>
+                    <img src="@asset('images/inc-logo.png')" alt="">
+                </li>
+                <li class="testimonials__item">
+                    <p>
+                        "The most helpful Amazon software in the space"
+                    </p>
+                    <img src="@asset('images/wall-street-journal-logo-vector-logo-wsj.png')" alt="">
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="section section--pricing">
+        <div class="container">
+            <h1 class="heading">
+                Save weeks of Amazon product research
+            </h1>
+            <p class="intro">
+                Try it risk-free! We have a 14-day money-back guarantee.
+            </p>
+            <div class="pricing">
+                <div class="pricingItem pricingItem--popular">
+                    <h2 class="pricingItem__heading">
+                        Seller Started Bundle
+                    </h2>
+                    <p class="pricingItem__text">
+                        Valued at $572
+                    </p>
+                    <p class="pricingItem__price">
+                        <sup>$</sup>
+                        <span>279</span>
+                    </p>
+                    <p class="pricingItem__text">
+                        Save over 40%!
+                    </p>
+                    <ul class="pricingItem__list">
+                        <li class="pricingItem__list__item">
+                            <span>
+                                3-month Standad WebApp
+                            </span>
+                            <ul class="pricingItem__sublist">
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                    </ul>
+                    <a href="#" class="pricingItem__button btn btn--lg">
+                        Choose Starter Bundle
+                    </a>
+                </div>
+                <div class="pricingItem">
+                    <h2 class="pricingItem__heading">
+                        Seller Started Bundle
+                    </h2>
+                    <p class="pricingItem__text">
+                        Valued at $572
+                    </p>
+                    <p class="pricingItem__price">
+                        <sup>$</sup>
+                        <span>279</span>
+                    </p>
+                    <p class="pricingItem__text">
+                        Save over 40%!
+                    </p>
+                    <ul class="pricingItem__list">
+                        <li class="pricingItem__list__item">
+                            <span>
+                                3-month Standad WebApp
+                            </span>
+                            <ul class="pricingItem__sublist">
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                    </ul>
+                    <a href="#" class="pricingItem__button btn btn--lg">
+                        Choose Starter Bundle
+                    </a>
+                </div>
+                <div class="pricingItem">
+                    <h2 class="pricingItem__heading">
+                        Seller Started Bundle
+                    </h2>
+                    <p class="pricingItem__text">
+                        Valued at $572
+                    </p>
+                    <p class="pricingItem__price">
+                        <sup>$</sup>
+                        <span>279</span>
+                    </p>
+                    <p class="pricingItem__text">
+                        Save over 40%!
+                    </p>
+                    <ul class="pricingItem__list">
+                        <li class="pricingItem__list__item">
+                            <span>
+                                3-month Standad WebApp
+                            </span>
+                            <ul class="pricingItem__sublist">
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                                <li class="pricingItem__sublist__item">
+                                    Keyword Scout
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                        <li class="pricingItem__list__item">
+                            <span>
+                                <b>FREE</b> Sourcing Bible ($69 Value)
+                            </span>
+                        </li>
+                    </ul>
+                    <a href="#" class="pricingItem__button btn btn--lg">
+                        Choose Starter Bundle
+                    </a>
+                </div>
+            </div>
+            <p class="intro">
+                Looking for a different product? Head over to our <a href="#">Pricing Page</a>!
+            </p>
+        </div>
+    </section>
+    <hr class="sectionDivider" />
+    <section class="section section--products">
+        <div class="container">
+            <h1 class="heading">
+                Four powerful tools in one
+            </h1>
+            <p class="intro">
+                Minimize risks and maximize your profits with Jungle Scout. With just a few clicks, you'll son be on your way to launching a successful Amazon FBA product.
+            </p>
+            <div class="media">
+                <figure class="media__figure">
+                    <img src="@asset('images/img_product-tracker.png')" alt="">
+                </figure>
+                <div class="media__text">
+                    <h3 class="media__text__heading">
+                        Spy on your competitors
+                    </h3>
+                    <h2>
+                        Product Tracker
+                    </h2>
+                    <p>
+                        Store and analyze your ideas over time. You can get historical data on products you wan to sell, as well as your competitors products.
+                    </p>
+                    <ul class="list">
+                        <li class="list__item">
+                            See the real daily sales of your competitors' products
+                        </li>
+                        <li class="list_item">
+                            Avoid temporary fluctuations in estimations
+                        </li>
+                        <li class="list__item">
+                            Keep tabs on your competitors throughout your product's lifecycle
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="media media--rightSideMedia">
+                <figure class="media__figure">
+                    <img src="@asset('images/img_product-tracker.png')" alt="">
+                </figure>
+                <div class="media__text">
+                    <h3>
+                        Spy on your competitors
+                    </h3>
+                    <h2>
+                        Product Tracker
+                    </h2>
+                    <p>
+                        Store and analyze your ideas over time. You can get historical data on products you wan to sell, as well as your competitors products.
+                    </p>
+                    <ul class="list">
+                        <li class="list__item">
+                            See the real daily sales of your competitors' products
+                        </li>
+                        <li class="list_item">
+                            Avoid temporary fluctuations in estimations
+                        </li>
+                        <li class="list__item">
+                            Keep tabs on your competitors throughout your product's lifecycle
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section section--features">
+        <div class="container">
+            <ul class="featureList">
+                <li class="featureList__item">
+                    <img src="@asset('images/icon_finger-snap.png')" alt="Easy to Use">
+                    <h3>
+                        Easy to use
+                    </h3>
+                    <p>
+                        No only is Jungle Scout easy to use, but we offer free educational resources and 24/7 customer support to ensure your success.
+                    </p>
+                </li>
+                <li class="featureList__item">
+                    <img src="@asset('images/icon_piggy-bank.png')" alt="">
+                    <h3>
+                        Easy to use
+                    </h3>
+                    <p>
+                        No only is Jungle Scout easy to use, but we offer free educational resources and 24/7 customer support to ensure your success.
+                    </p>
+                </li>
+                <li class="featureList__item">
+                    <img src="@asset('images/icon_pc-shield.png')"  alt="">
+                    <h3>
+                        Easy to use
+                    </h3>
+                    <p>
+                        No only is Jungle Scout easy to use, but we offer free educational resources and 24/7 customer support to ensure your success.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <hr class="sectionDivider" />
+    <section class="section section--cta">
+        <div class="container text-center">
+            <h1 class="heading">
+                Join the 117,382 Amazon sellers who run their business with Jungle Scout
+            </h1>
+            <p>
+                Risk free, 100% money back guarantee.
+            </p>
+            <a href="#" class="btn">
+                Get started now
+            </a>
+        </div>
+    </section>
+    @include('partials.content-page')
+  @endwhile
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="content-info bg-orange">
+  <div class="container">
+    @php dynamic_sidebar('sidebar-footer') @endphp
+  </div>
+</footer>
